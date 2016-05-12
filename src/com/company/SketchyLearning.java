cae3f73c371a25daf4311a78ecda17a999c12c9f
Max Vigdorchik <maxv008@gmail.com>
2016-05-12 15:03:23 -0400

    Small Update

---
+++ src/com/company/SketchyLearning.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by maxv0 on 5/6/2016.
+ */
+public class SketchyLearning
+{
+    int ssInput;
+
+    public SketchyLearning()
+    {
+        ssInput = 1;
+    }
+
+
+}
