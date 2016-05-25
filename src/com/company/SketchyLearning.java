e93b996ea53209586cab61c0417c5e20311801c2
Max Vigdorchik <maxv008@gmail.com>
2016-05-24 20:27:54 -0400

    Changed the way math is handled so that the first few similarities count less but further ones count more.

--- src/com/company/SketchyLearning.java
+++ src/com/company/SketchyLearning.java
@@ -11,11 +11,13 @@ public class SketchyLearning //IGNORE THIS FOR NOW, JUST A PLACEHOLDER.
 {
     public List<Double> constants;
     public List<Project> projects;
+    public List<ProjectPair> pairList;
 
-    public SketchyLearning(List<Project> projects)
+    public SketchyLearning(List<Project> projects, List<ProjectPair> pairList)
     {
         constants = new ArrayList<>();
         this.projects = projects;
+        this.pairList = pairList;
     }
 
 
